e821640d28a3d714f3f42878a123475e71c184a4
alnumanqa <alnumanqa@gmail.com>
2022-11-07 07:56:01 -0500

    HW6

---
+++ src/hw6IfElseCondition01/Voter.java
@@ -0,0 +1,19 @@
+package hw6IfElseCondition01;
+
+public class Voter {
+	public static void main(String[] args) {
+		int age = 29;
+
+		if (age == 18) {
+			System.out.println("I am a voter");
+		} else if (age < 18) {
+			System.out.println("I am not a voter");
+		} else if (age > 18) {
+			System.out.println("I am a voter");
+		} else {
+			System.out.println("Please add a valid age");
+		}
+
+	}
+
+}
---
+++ src/hw6IfElseCondition02/Temperature.java
@@ -0,0 +1,26 @@
+package hw6IfElseCondition02;
+
+import java.util.Scanner;
+
+public class Temperature {
+
+	public static void main(String[] args) {
+		System.out.println("Todays Weather");
+		Scanner scanner = new Scanner(System.in);
+		int todaysTemperature = scanner.nextInt();
+
+		if (todaysTemperature < 32) {
+			System.out.println("Freezing");
+		} else if (todaysTemperature < 55) {
+			System.out.println("Pleasent");
+		} else if (todaysTemperature < 73) {
+			System.out.println("Getting Warmer");
+		} else if (todaysTemperature > 101) {
+			System.out.println("Hot");
+		} else {
+			System.out.println("Please put ice on my head");
+		}
+
+	}
+
+}
